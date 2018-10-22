4cb34ca6975c305e930dc66b14de12e113d034a3
E. Bamba <ebamba@bitsnshine.com>
2018-10-21 22:01:14 -0400

    Minor

--- src/custom_worker.js
+++ src/custom_worker.js
@@ -2,7 +2,7 @@ export default function customServiceWorker() {
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker.register('/custom_sw.js', {scope: '/'})
       .then((register) =>{
-        console.log('Registration succeeded!', register);
+        // console.log('Registration succeeded!', register);
       })
       .catch((error) =>{
         console.log('Registration failed with ' + error);
